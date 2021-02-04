ada45b4bf5c7b2b7635c5b6e1766d55ef77a2c53
Ahmad Lufi <ahmadlufi300@gmail.com>
2021-02-04 11:07:13 +0700

    Field Revisited

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -5,7 +5,9 @@ import * as Yup from 'yup'
 const initialValues = {
     name: '',
     email: '',
-    channel: ''
+    channel: '',
+    commenst: '',
+    address: ''
 }
 
 const onSubmit = values => {
@@ -69,8 +71,31 @@ function YoutubeForm() {
 
                 <div className='form-control'>
                     <label htmlFor='channel'>Channel</label>
-                    <Field type='text' id='channel' name='channel' />
+                    <Field type='text' id='channel' name='channel' placeholder='Youtube Channel name' />
                     <ErrorMessage name='channel' />
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor='comments'>Channel</label>
+                    <Field as='textarea' id='comments' name='comments' />
+                </div>
+
+                <div className='form-control'>
+                    <label htmlFor='address'>Channel</label>
+                    <Field name='address' >
+                        { 
+                            (props) => {
+                                const { field, form, meta } = props
+                                console.log('Render props', props)
+                                return (
+                                    <div>
+                                        <input type='text' id='address' {...field} />
+                                        {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                                    </div>
+                                )
+                            }
+                        }
+                    </Field>
                 </div>
 
                 <button type='submit'>Submit</button>
