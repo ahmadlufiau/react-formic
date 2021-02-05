593ac19207af6d03e09c1cfecdf3887298852b14
Ahmad Lufi A U <ahmadlufi300@gmail.com>
2021-02-05 20:13:53 +0700

    Field Level Validation

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -52,6 +52,15 @@ const validationSchema = Yup.object({
     channel: Yup.string().required('Required!'),
 })
 
+const validateComments = value => {
+    let error
+
+    if(!value) {
+        error = 'Required!'
+    }
+    return error
+}
+
 function YoutubeForm() {
 
     // console.log('Form values', formik.values)
@@ -63,8 +72,8 @@ function YoutubeForm() {
             initialValues={initialValues}
             validationSchema={validationSchema}
             onSubmit={onSubmit}
-            validateOnChange={false}
-            validateOnBlur={false}
+            // validateOnChange={false}
+            // validateOnBlur={false}
             >
             <Form>
                 <div className='form-control'>
@@ -91,7 +100,8 @@ function YoutubeForm() {
 
                 <div className='form-control'>
                     <label htmlFor='comments'>Comments</label>
-                    <Field as='textarea' id='comments' name='comments' />
+                    <Field as='textarea' id='comments' name='comments' validate={validateComments} />
+                    <ErrorMessage name='comments' component={TextError} />
                 </div>
 
                 <div className='form-control'>
