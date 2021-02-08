cb5ce3a87c37f324d2a6e80f2f38a3fa1efcdc99
Ahmad Lufi <ahmadlufi300@gmail.com>
2021-02-08 22:53:23 +0700

    Radio buttons

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -10,15 +10,22 @@ function FormikContainer() {
         { key: 'Option 2', value: 'option2'},
         { key: 'Option 3', value: 'option3'}
     ]
+    const radioButtons = [
+        { key: 'Option 1', value: 'rOption1'},
+        { key: 'Option 2', value: 'rOption2'},
+        { key: 'Option 3', value: 'rOption3'}
+    ]
     const initialValues = {
         email: '',
         description: '',
-        selectOption: ''
+        selectOption: '',
+        radioOption: ''
     }
     const validationSchema = Yup.object({
         email: Yup.string().required('Required'),
         description: Yup.string().required('Required'),
-        selectOption: Yup.string().required('Required')
+        selectOption: Yup.string().required('Required'),
+        radioOption: Yup.string().required('Required')
     })
     const onSubmit = values => console.log('Form data', values)
     return (
@@ -32,6 +39,7 @@ function FormikContainer() {
                     <FormikControl control='input' type='email' label='email' name='email' />
                     <FormikControl control='textarea' label='description' name='description' />
                     <FormikControl control='select' label='Select a topic' name='selectOption' options={dropdownOptions} />
+                    <FormikControl control='radio' label='Radio Topic' name='radioOption' options={radioButtons} />
                     <button type='submit'>Submit</button>
                 </Form>
             }
