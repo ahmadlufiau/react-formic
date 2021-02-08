94ef31e43a1efcf67de0170dd769a8565a36680d
Ahmad Lufi <ahmadlufi300@gmail.com>
2021-02-08 23:51:05 +0700

    Registration Form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 // import YoutubeForm from './components/YoutubeForm';
 // import FormikContainer from './components/FormikContainer'
-import LoginForm from './components/LoginForm'
+// import LoginForm from './components/LoginForm'
+import RegistrationForm from './components/RegistrationForm'
 
 function App() {
   return (
     <div className="App">
       {/* <YoutubeForm /> */}
       {/* <FormikContainer /> */}
-      <LoginForm />
+      <RegistrationForm />
     </div>
   );
 }
